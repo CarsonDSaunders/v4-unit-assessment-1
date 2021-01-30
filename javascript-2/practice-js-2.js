7448f35ac67547fa0f421d09ec8c67ac2fb6fcd4
Carson Saunders <carsonsaundersalt@gmail.com>
2021-01-30 13:27:36 -0700

    Completed JS part 2

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -9,7 +9,7 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+let gitDefinition = 'Git is a version control software/set of tools that allows for code to easily be edited, merged, reviewed, and collaborated on.'
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,7 +17,7 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+let gitHubDefinition = 'GitHub is an online platform that integrates with Git to store projects in the cloud. It has tons of useful features and is one of the staples of the industry.'
 
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
@@ -33,7 +33,10 @@
     following the guidelines above to describe the init command.
 */
 
-//CODE HERE
+let init = {
+    description: 'Creates a Git repo in the current directory',
+    code: 'git init',
+}
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -41,7 +44,10 @@
     following the guidelines above to describe the clone command.
 */
 
-//CODE HERE
+let clone = {
+    description: 'Creates a directory corresponding to a Git repo and initializes it',
+    code: 'git clone [url]',
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -49,7 +55,10 @@
     following the guidelines above to describe the status command.
 */
 
-//CODE HERE
+let status = {
+    description: 'Shows a list of changes that have happened to files in the current repo',
+    code: 'git status',
+}
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -57,7 +66,10 @@
     following the guidelines above to describe the add command.
 */
 
-//CODE HERE
+let add = {
+    description: 'Stages the specified files for a commit',
+    code: 'git add [files (usually .)]',
+}
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -65,7 +77,10 @@
     following the guidelines above to describe the commit command.
 */
 
-//CODE HERE
+let commit = {
+    description: 'Copies staged file changes over to the current branch in preperation for a push',
+    code: 'git commit -m [message]',
+}
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,7 +88,10 @@
     following the guidelines above to describe the command to add a remote location to your git repository.
 */
 
-//CODE HERE
+let addRemote = {
+    description: 'Adds the current local repo to a remote repo (almost like a reverse of the clone command)',
+    code: 'git remote add origin [url]',
+}
 
 //////////////////PROBLEM 9////////////////////
 /*
@@ -81,4 +99,7 @@
     following the guidelines above to describe the push command.
 */
 
-//CODE HERE
+let push = {
+    description: 'Uploads any commited changes to the Git repo',
+    code: 'git push origin master',
+}
